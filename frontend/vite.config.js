9ca02c30e95ac42af510d27665b74a79e5589907
alex l <alexliao365@gmail.com>
2025-03-28 21:45:35 -0700

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import 'dotenv/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
   }
 });
