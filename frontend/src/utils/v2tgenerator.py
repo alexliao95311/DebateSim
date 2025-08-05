7b5d62d0b71f9a4365f04cb7b6aeab8c3fcf8e8d
MrinalA2009 <smmrinal2009@gmail.com>
2025-08-04 22:25:34 -0700

    started mic stream, need main

--- frontend/src/utils/v2tgenerator.py
+++ frontend/src/utils/v2tgenerator.py
@@ -4,3 +4,12 @@ from google.cloud import speech
 
 RATE = 16000
 CHUNK = int(RATE / 10) # btw this means 100ms
+
+class MicrophoneStream:
+    """rec system to chunk"""
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
+
+        self._buff = queue.Queue()
+        self.closed = True
